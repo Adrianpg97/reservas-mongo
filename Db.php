df881b33c88f0e2e039503b0e9916ba5f1801069
Axel <firestormaxel@gmail.com>
2018-12-17 20:00:29 +0100

    Comentario conexion nueva

--- Db.php
+++ Db.php
@@ -11,6 +11,7 @@
 				//Esto es una prueba de conexion 2
 				$conexion = new MongoDB\Client;
 				//Seleccionamos base de datos
+				//Prueba conexionn mongo Db
 				self::$conexion = $conexion->pruebas;
 
 				return self::$conexion;
