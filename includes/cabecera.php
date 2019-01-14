267829360f9053b6ab4a5c39b4d13c36168905d0
Adrian Peña Gimenez <adri.pulpi@gmail.com>
2019-01-14 20:21:37 +0100

    Carpetas con los ejercicios

--- includes/cabecera.php
+++
@@ -1,9 +0,0 @@
-
-<!DOCTYPE html>
- <html lang="en">
-	 <head>
-		 <meta charset="UTF-8">
-		 <title>Agenda de contactos</title>
-		 <link rel="stylesheet" href="css/estilos.css">
-	 </head>
-	 <body>
