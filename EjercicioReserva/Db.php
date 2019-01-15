34d5e2924f2a4b90d058219e6cfdd1ac6b92bc14
Adrian <adri.pulpi@gmail.com>
2019-01-15 12:13:27 +0100

    Reservas

---
+++ EjercicioReserva/Db.php
@@ -0,0 +1,19 @@
+<?php
+	require 'vendor/autoload.php';
+
+	class  Db{
+		private static $conexion=NULL;
+		private function __construct (){}
+ 
+		public static function conectar(){
+
+				//Abrimos conexión a Mongo
+				//Esto es una prueba de conexion 2
+				$conexion = new MongoDB\Client;
+				//Seleccionamos base de datos
+				self::$conexion = $conexion->reservas;
+
+				return self::$conexion;
+		}		
+	} 
+?>
